b4082227e7c64ad383e71933ab2876be41184e48
AsadIqbal <asad.iqbal6019@gmail.com>
2022-10-06 11:09:01 +0500

    my first commit

---
+++ src/main/java/com/cloudflashdemo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.cloudflashdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+	
+	@GetMapping("/demo")
+    public String sayHello(){
+        
+        return "Hello-world";
+    }
+
+
+}
+	
+	
+
+
